58466f23bd53a7b376f08bde497ef923771f8804
Artur Machnacki <machnacki_artur@onet.eu>
2018-04-04 18:47:10 +0000

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.set('port', process.env.PORT || 5000);
 
 app.use(express.static(__dirname + '/dist'));
 app.get('/*', function(req, res) { 
-  res.sendFile(__dirname + '/index.html')
+  res.sendFile(__dirname + '/dist/index.html')
 });
 
 app.listen(app.get('port'), function () {
