315d8ab9ad7ac0884de9254f847750b653fb610f
Artur Machnacki <machnacki_artur@onet.eu>
2018-04-04 17:52:54 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,12 @@
-var http = require("http");
+var express = require('express');
+var app = express();
+app.set('port', process.env.PORT || 5000);
 
-function onRequest(req, res) {
-	if (req.url == "/") {
-		res.writeHead(200, { "Content-Type": "text/html" });
-		res.end("Welcome to the Taxihub homepage!");
-	}
-	
-	// About page
-	else if (req.url == "/contact") {
-		res.writeHead(200, { "Content-Type": "text/html" });
-		res.end("Welcome to the contact page of Taxihub!");
-	}
-	
-	// 404'd!
-	else {
-		res.writeHead(404, { "Content-Type": "text/plain" });
-		res.end("404! Page not found.");
-	}
-}
 
-http.createServer(onRequest).listen(9000);
-console.log("Server has started.");
+app.get('/', function (req, res) {
+  res.send('Welcome to TaxiHub');
+});
+
+app.listen(app.get('port'), function () {
+  console.log('App is listening on port ' + app.get('port'));
+});
