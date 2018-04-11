0febcd80d598a845a80368fb52cd6c77425e83ed
Bartosz Rachwalik <rachwalikbartosz@gmail.com>
2018-04-11 19:19:24 +0200

    improved orders history display

--- src/app/order/order.model.ts
+++ src/app/order/order.model.ts
@@ -6,8 +6,8 @@ export class Order {
   public fromLongitude: number;
   public toLatitude: number;
   public toLongitude: number;
-  public createTime: string;
-  public takenTime: string;
+  public openTime: string;
+  public startTime: string;
   public endTime: string;
   public status: string;
 
@@ -19,18 +19,4 @@ export class Order {
     this.toLongitude = toLongitude;
   }
 
-  // constructor(id: number, driverId: number, clientId: number, fromLatitude: number, fromLongitude: number, toLatitude: number,
-  //             toLongitude: number, createTime: string, takenTime: string, endTime: string, status: string) {
-  //   this.id = id;
-  //   this.driverId = driverId;
-  //   this.clientId = clientId;
-  //   this.fromLatitude = fromLatitude;
-  //   this.fromLongitude = fromLongitude;
-  //   this.toLatitude = toLatitude;
-  //   this.toLongitude = toLongitude;
-  //   this.createTime = createTime;
-  //   this.takenTime = takenTime;
-  //   this.endTime = endTime;
-  //   this.status = status;
-  // }
 }
