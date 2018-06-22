299ef1e4f168a82243b2c79be94041295f13b613
Bartosz Rachwalik <rachwalikbartosz@gmail.com>
2018-06-22 17:09:33 +0200

    confirm form reworked

--- src/app/confirm/confirm.component.ts
+++ src/app/confirm/confirm.component.ts
@@ -1,15 +1,17 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, DoCheck, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {DriverService} from '../driver/driver.service';
 import {NotificationService} from '../notification/notification.service';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-confirm',
   templateUrl: './confirm.component.html',
   styleUrls: ['./confirm.component.css']
 })
-export class ConfirmComponent implements OnInit {
-
+export class ConfirmComponent implements OnInit, DoCheck {
+  passwordConfirmed = false;
+  confirmForm: FormGroup;
   token: string;
 
   constructor(private route: ActivatedRoute,
@@ -19,7 +21,15 @@ export class ConfirmComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.confirmForm = new FormGroup({
+      'password': new FormControl('', [Validators.required, Validators.minLength(6)]),
+      'confirm': new FormControl('', [Validators.required, Validators.minLength(6)]),
+    });
     this.token = this.route.snapshot.params['token'];
+  }
+
+  ngDoCheck() {
+    this.passwordConfirmed = this.confirmForm.get('password').value === this.confirmForm.get('confirm').value;
   }
 
   sendPassword(password: string) {
